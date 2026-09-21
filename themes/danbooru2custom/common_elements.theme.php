ce1b708a576c16160f83e8ac0b2653cc07554c51
Filip <git@sl.terrails.com>
2026-09-21 22:22:04 +0200

    feature: add blue border to video posts and display video duration on thumbnail

--- themes/danbooru2custom/common_elements.theme.php
+++ themes/danbooru2custom/common_elements.theme.php
@@ -10,6 +10,67 @@ use MicroHTML\HTMLElement;
 
 class Danbooru2CustomCommonElementsTheme extends CommonElementsTheme
 {
+    /**
+     * CUSTOMIZATION START: Override build_thumb to add video duration marker
+     */
+    public function build_thumb(Post $image): HTMLElement
+    {
+        // Get the parent's thumbnail
+        $thumb = parent::build_thumb($image);
+        
+        $mime = $image->get_mime();
+        $mime_str = (string)$mime;
+        $is_video = strpos($mime_str, "video/") === 0;
+        
+        if (!$is_video) {
+            return $thumb;
+        }
+        
+        // Extract attributes and children using reflection
+        $reflection = new \ReflectionClass($thumb);
+        $attrs_prop = $reflection->getProperty('attrs');
+        $children_prop = $reflection->getProperty('children');
+        $attrs_prop->setAccessible(true);
+        $children_prop->setAccessible(true);
+        
+        $attrs = $attrs_prop->getValue($thumb);
+        $children = $children_prop->getValue($thumb);
+        
+        // Add video class
+        $attrs['class'] = ($attrs['class'] ?? '') . ' video';
+        
+        // Rebuild the anchor with modified attributes
+        $thumb = A($attrs, ...$children);
+        
+        // Try to add duration and audio badge
+        if ($image->length !== null && $image->length > 0) {
+            $total_seconds = (int)floor($image->length / 1000);
+            $hours = (int)floor($total_seconds / 3600);
+            $minutes = (int)floor(($total_seconds % 3600) / 60);
+            $seconds = $total_seconds % 60;
+
+            if ($hours > 0) {
+                $duration_text = sprintf("%d:%02d:%02d", $hours, $minutes, $seconds);
+            } else {
+                $duration_text = sprintf("%d:%02d", $minutes, $seconds);
+            }
+            
+            // Add audio icon if video has audio
+            if ($image->audio === true) {
+                $duration_text .= " 🔊";
+            }
+            
+            $duration_badge = DIV(
+                ["class" => "video-duration"],
+                $duration_text
+            );
+            $thumb->appendChild($duration_badge);
+        }
+        
+        return $thumb;
+    }
+    /* CUSTOMIZATION END */
+
     public function display_paginator(string $base, ?QueryArray $query, int $page_number, int $total_pages, bool $show_random = false): void
     {
         if ($total_pages === 0) {
